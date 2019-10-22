4956deba3a9c766cb4b88b1ceb5e56c2d1c220a3
Ygor Mattos <ygormattos.b@gmail.com>
2019-10-22 14:49:18 -0300

    :rocket: Criando sessões com JWT Token

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '31cf941b0a415a0c11e2cde0970c3a44',
+  expiresIn: '7d'
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
+// get
 routes.get('/users', UserController.index);
+
+// post
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// update
+
+// delete
+
 
 export default routes;
